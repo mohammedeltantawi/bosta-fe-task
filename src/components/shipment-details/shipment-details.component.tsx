5d225e677299abd442dd4e3c2c0d8c6c5579f43b
motantawy <motantawy@alinma.com>
2024-05-01 16:03:14 +0300

    add reason in shipment details table

--- src/components/shipment-details/shipment-details.component.tsx
+++ src/components/shipment-details/shipment-details.component.tsx
@@ -1,14 +1,23 @@
 import { t } from 'i18next';
+import { TransitEventsState } from '../../enums/transit-events.enum';
 import { ShipmentTrackResponse } from '../../models/shipment-track-response.model';
 import { getDateNumbers, getTime } from '../../services/global-functions';
-
+import "./shipment-details.style.css";
 interface TrackerCardProps {
     shipmentDetails: ShipmentTrackResponse;
 }
 const ShipmentDetails = ({ shipmentDetails }: TrackerCardProps) => {
   
-  const rowClass="flex flex-row h-auto items-center p-5 border-solid border-b "
-  const rowCell= "w-1/4 text-wrap"
+  const rowClass="flex flex-row h-auto items-center p-5 border-solid border-b";
+  const rowCell= "w-1/4 text-wrap";
+
+  const getShipmentDetailsStatusClass= (step?: number): string => {
+    switch(shipmentDetails.CurrentStatus.state) {
+        case (TransitEventsState.DELIVERED): return "success-color";
+        case (TransitEventsState.CANCELLED): return"cancelled-color";
+        default: return "waiting-color";
+    }
+}
   return (
     <div className="flex flex-col gap-4 w-full md:w-2/3">
       <p className='card-title'>{t('shipmentDetails.shipmentDetails')}</p>
@@ -39,9 +48,14 @@ const ShipmentDetails = ({ shipmentDetails }: TrackerCardProps) => {
             <p className={rowCell}>
               {getTime(event.timestamp)}
             </p>
-            <p className={rowCell}>
-              {t(`transitStatus.${event.state}`)}
-            </p>
+            <div className={`flex flex-col gap-1 ${rowCell}`}> 
+              <p>
+                {t(`transitStatus.${event.state}`)}
+              </p>
+              <p className={`reason-text ${getShipmentDetailsStatusClass()}`}>
+                {t(`${event.reason ?? ""}`)}
+              </p>
+            </div>
           </div>)
          })
         }
